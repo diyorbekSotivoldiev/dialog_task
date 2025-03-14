f694bfd4409f4e7f5dce11b56a2ac253ae703d3f
diyorbekSotivoldiev <sotivoldievdiyorbek@gmail.com>
2025-03-14 17:06:29 +0500

    dialog widget

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+/// Flutter code sample for [AlertDialog].
+
+void main() => runApp(const AlertDialogExampleApp());
+
+class AlertDialogExampleApp extends StatelessWidget {
+  const AlertDialogExampleApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text(
+            'Dialog Topshiriq',
+            style: TextStyle(color: Colors.white),
+          ),
+          backgroundColor: Colors.blueAccent,
+        ),
+        body: const Center(child: DialogExample()),
+      ),
+    );
+  }
+}
+
+class DialogExample extends StatelessWidget {
+  const DialogExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () => showDialog<String>(
+        context: context,
+        builder: (BuildContext context) => AlertDialog(
+          title: const Text('Dialogga misol'),
+          content: const Text(
+              'AlertDialog Dialog bilan birga ishlatiladigan vidjet'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () => Navigator.pop(context, 'OK'),
+              child: const Text('OK'),
+            ),
+          ],
+        ),
+      ),
+      child: const Text("Dialogni Ko'rsat"),
+    );
+  }
+}
